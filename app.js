88c2168413357b1b7e8135510e21b5e76f6bd463
PKILB <paytonboone30@gmail.com>
2023-01-26 23:46:48 -0700

    Tired Boy

--- app.js
+++ app.js
@@ -27,11 +27,40 @@ function drawHero() {
 }
 drawHero()
 
+function drawHeroStats() {
+    health.value -= boss.damage
+    let heroStatsTemplate = ''
+    heroStatsTemplate += `
+    <div class="row">
+    <div class="col-10 py-3">
+        <progress value="100" max="100"></progress>
+    </div>
+    <div class="col-10 py-3">
+    <p class="pt-3">${hero.damage}</p>
+    </div>
+    `
+
+    console.log(heroStatsTemplate);
+    document.getElementById('hero-stats').innerHTML = heroStatsTemplate
+}
+
 function drawBoss() {
     let bosstemplate = ''
     bosstemplate += `
-    <img class="boss-size" src="${boss.image}" alt="">
+    <img class="boss-size" src="${boss.image}" alt="" onclick="attackBoss">
     `
     document.getElementById('boss').innerHTML = bosstemplate
 }
 drawBoss()
+
+function drawBossStats() {
+
+    document.getElementById('boss-stats')
+}
+
+
+function attackBoss() {
+    drawHeroStats()
+
+    boss.health -= hero.damage
+}
